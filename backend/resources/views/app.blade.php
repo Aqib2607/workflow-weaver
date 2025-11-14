960b16aa6ca593825cd81c0028a22a3a8c5d17fe
Aqib Jawwad <aqibjawwad2607@gmail.com>
2025-11-15 02:31:28 +0600

    🔗 Laravel Frontend Integration - Serve React Homepage

---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlowBuilder</title>
+    <script type="module" crossorigin src="{{ asset('assets/index-B9d2UHFx.js') }}"></script>
+    <link rel="stylesheet" crossorigin href="{{ asset('assets/index-Dqti5t8q.css') }}">
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
